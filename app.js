a07bd3223eb38ec07334a967fa091a6e8b481525
Jacob Marshall <jacob+github@manage.net.nz>
2015-04-20 21:22:58 +1200

    Added X-Powered-By header to test

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    port = process.env.PORT || 8000 + Math.floor(Math.random() * 100);
+
+app.use(function (req, res, next) {
+    res.set('X-Powered-By', 'mnet-io');
+    next();
+});
 
 app.use(express.static(__dirname + '/public'));
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || port, function () {
+    console.log('Listening for connections on :%s', port);
+});
